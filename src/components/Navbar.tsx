fe25f9f0203ea3048b7b1cc495d9b18e0f4f706d
Dale B <daleban.dev@gmail.com>
2021-08-22 17:43:44 +0800

    Design navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,96 @@
-import React from "react";
+/**@jsx jsx */
+import { css, jsx } from '@emotion/react'
+import React, { useState } from "react";
+
+
+import { CgMenuRight } from 'react-icons/cg'
+import { FiSearch } from 'react-icons/fi'
+
+import logo from '../img/Marvel_Logo.png'
 
 export const Navbar: React.FC = () => {
+    const [clicked, setClicked] = useState<string>('')
+
     return (
-        <div>
+        <nav className='Navbar'>
+        <div 
+        css={css`
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            max-width: 95%;
+            margin: 0 auto;
+            padding: 20px 0;
+
+            ul {
+                display: flex;
+                list-style: none;
+                font-size: 1.5rem;
+
+                li {
+                color: #9C9A9A;
+                text-transform: uppercase;
+                cursor: pointer;
+
+                    &:not(:last-child) {
+                        margin-right: 2.4rem;
+                    }
+                }
+
+                .clicked-btn {
+                    color: #fff;
+
+                    &::after {
+                        content: "";
+                        display: block;
+                        width: 7px;
+                        height: 7px;
+                        position: relative;
+                        bottom: 4px;
+                        border-radius: 50%;
+                        background: #ED1D24;
+                        margin: 0 auto;
+                    }
+                }
+            }
+
+            .icons {
+                display: flex;
+
+                
+                .icon {
+                    display: flex;
+                    color: #fff;
+                    font-size: 1.9rem;
+                    margin-left: 2rem;
+                    cursor: pointer;
+                }
+
+                .icon-search {
+                    font-size: 1.7rem;
+                    color: #9C9A9A;
+                }
+            }
+
+            .marvel-logo {
+                height: 2.4rem;
+                margin-right: 3rem;
+            }
+        `}
+        >
             <ul>
-                <li>home</li>
-                <li>videos</li>
-                <li>comics</li>
-                <li>movies</li>
-                <li>tv shows</li>
+                <img className='marvel-logo' src={logo} alt='Marvel Logo' />
+                <li className={clicked === 'home' ? 'clicked-btn' : ''} onClick={() => setClicked('home')} >home</li>
+                <li className={clicked === 'videos' ? 'clicked-btn' : ''} onClick={() => setClicked('videos')} >videos</li>
+                <li className={clicked === 'comics' ? 'clicked-btn' : ''} onClick={() => setClicked('comics')}>comics</li>
+                <li className={clicked === 'movies' ? 'clicked-btn' : ''} onClick={() => setClicked('movies')}>movies</li>
+                <li className={clicked === 'shows' ? 'clicked-btn' : ''} onClick={() => setClicked('shows')}>tv shows</li>
             </ul>
+            <div className="icons">
+            <FiSearch className='icon icon-search' />
+            <CgMenuRight className='icon icon-menu' />
+            </div>
         </div>
+        </nav>
     )
 }
