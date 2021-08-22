951af8960fb95a0eee729b439abcf6e058185c51
Dale B <daleban.dev@gmail.com>
2021-08-22 15:48:13 +0800

    Import images in react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 /**@jsx jsx */
 import { css, jsx, Global } from '@emotion/react'
-
 import React, { useState } from 'react'
+
+import background from './img/background.jpg'
+import titlecard from './img/title-card.jpg'
+import logo from './img/Marvel_Logo.png'
+import scene0 from './img/scene0.jpg'
+import scene1 from './img/scene1.jpg'
+import scene2 from './img/scene2.jpg'
+
 
 const App: React.FC = () => {
 
   return (
-    <div className="App">
+    <div 
+    className="App"
+    css={css`
+      
+    `}
+    >
      <span></span>
      <Global 
      styles={css`
