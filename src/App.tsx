c963f52976056642e80efa28e21119e997726e93
Dale B <daleban.dev@gmail.com>
2021-08-23 15:34:28 +0800

    Finzalize web project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
 /**@jsx jsx */
-import { css, jsx, Global } from '@emotion/react'
-import React, { useState } from 'react'
+import { css, jsx, Global } from "@emotion/react";
+import React, { useState } from "react";
 
-import { Navbar } from './components/Navbar'
-import { SectionLeft } from './components/SectionLeft'
-import { SectionRight } from './components/SectionRight'
+import { Navbar } from "./components/Navbar";
+import { Section } from "./components/Section";
 
-import background1 from './img/background1.jpg'
+import background3 from "./img/background3.jpg";
 
 const App: React.FC = () => {
-
   return (
-    <div 
-    className="App"
-    css={css`
-      background: url(${background1});
-      background-size: cover;
-      height: 100vh;
-    `}
+    <div
+      className="App"
+      css={css`
+        background: url(${background3});
+        background-size: cover;
+        height: 100vh;
+
+        .flex-section {
+          display: flex;
+        }
+      `}
     >
-     <Navbar />
-     <div className="flex-section">
-     <SectionLeft />
-     <SectionRight />
-     </div>
-     <Global 
-     styles={css`
-        @import url('https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap');
+      <Navbar />
+      <Section />
+      <Global
+        styles={css`
+          @import url("https://fonts.googleapis.com/css2?family=Teko:wght@300;400&display=swap");
+          @import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200;300&display=swap");
 
-        * {
-        font-family: 'Teko', sans-serif;
-        margin: 0;
-        padding: 0;
-        }
+          * {
+            font-family: "Teko", sans-serif;
+            margin: 0;
+            padding: 0;
+          }
 
-        body {
-          background: #000;
-        }
-
-     `} />
+          body {
+            background: #000;
+          }
+        `}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,96 +1,119 @@
 /**@jsx jsx */
-import { css, jsx } from '@emotion/react'
+import { css, jsx } from "@emotion/react";
 import React, { useState } from "react";
 
+import { CgMenuRight } from "react-icons/cg";
+import { FiSearch } from "react-icons/fi";
 
-import { CgMenuRight } from 'react-icons/cg'
-import { FiSearch } from 'react-icons/fi'
-
-import logo from '../img/Marvel_Logo.png'
+import logo from "../img/Marvel_Logo.png";
 
 export const Navbar: React.FC = () => {
-    const [clicked, setClicked] = useState<string>('')
+  const [clicked, setClicked] = useState<string>("home");
 
-    return (
-        <nav className='Navbar'>
-        <div 
+  return (
+    <nav className="Navbar">
+      <div
         css={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          max-width: 90%;
+          margin: 0 auto;
+          padding: 20px 0;
+
+          ul {
             display: flex;
-            justify-content: space-between;
-            align-items: center;
-            max-width: 95%;
-            margin: 0 auto;
-            padding: 20px 0;
+            list-style: none;
+            font-size: 1.5rem;
 
-            ul {
-                display: flex;
-                list-style: none;
-                font-size: 1.5rem;
+            li {
+              color: #9c9a9a;
+              text-transform: uppercase;
+              cursor: pointer;
 
-                li {
-                color: #9C9A9A;
-                text-transform: uppercase;
-                cursor: pointer;
-
-                    &:not(:last-child) {
-                        margin-right: 2.4rem;
-                    }
-                }
-
-                .clicked-btn {
-                    color: #fff;
-
-                    &::after {
-                        content: "";
-                        display: block;
-                        width: 7px;
-                        height: 7px;
-                        position: relative;
-                        bottom: 4px;
-                        border-radius: 50%;
-                        background: #ED1D24;
-                        margin: 0 auto;
-                    }
-                }
+              &:not(:last-child) {
+                margin-right: 2.4rem;
+              }
             }
 
-            .icons {
-                display: flex;
+            .clicked-btn {
+              color: #fff;
 
-                
-                .icon {
-                    display: flex;
-                    color: #fff;
-                    font-size: 1.9rem;
-                    margin-left: 2rem;
-                    cursor: pointer;
-                }
+              &::after {
+                content: "";
+                display: block;
+                width: 7px;
+                height: 7px;
+                position: relative;
+                bottom: 4px;
+                border-radius: 50%;
+                background: #ed1d24;
+                margin: 0 auto;
+              }
+            }
+          }
 
-                .icon-search {
-                    font-size: 1.7rem;
-                    color: #9C9A9A;
-                }
+          .icons {
+            display: flex;
+
+            .icon {
+              display: flex;
+              color: #fff;
+              font-size: 1.9rem;
+              margin-left: 2rem;
+              cursor: pointer;
             }
 
-            .marvel-logo {
-                height: 2.4rem;
-                margin-right: 3rem;
+            .icon-search {
+              font-size: 1.7rem;
+              color: #9c9a9a;
             }
+          }
+
+          .marvel-logo {
+            height: 2.4rem;
+            margin-right: 3rem;
+          }
         `}
-        >
-            <ul>
-                <img className='marvel-logo' src={logo} alt='Marvel Logo' />
-                <li className={clicked === 'home' ? 'clicked-btn' : ''} onClick={() => setClicked('home')} >home</li>
-                <li className={clicked === 'videos' ? 'clicked-btn' : ''} onClick={() => setClicked('videos')} >videos</li>
-                <li className={clicked === 'comics' ? 'clicked-btn' : ''} onClick={() => setClicked('comics')}>comics</li>
-                <li className={clicked === 'movies' ? 'clicked-btn' : ''} onClick={() => setClicked('movies')}>movies</li>
-                <li className={clicked === 'shows' ? 'clicked-btn' : ''} onClick={() => setClicked('shows')}>tv shows</li>
-            </ul>
-            <div className="icons">
-            <FiSearch className='icon icon-search' />
-            <CgMenuRight className='icon icon-menu' />
-            </div>
+      >
+        <ul>
+          <img className="marvel-logo" src={logo} alt="Marvel Logo" />
+          <li
+            className={clicked === "home" ? "clicked-btn" : ""}
+            onClick={() => setClicked("home")}
+          >
+            home
+          </li>
+          <li
+            className={clicked === "videos" ? "clicked-btn" : ""}
+            onClick={() => setClicked("videos")}
+          >
+            videos
+          </li>
+          <li
+            className={clicked === "comics" ? "clicked-btn" : ""}
+            onClick={() => setClicked("comics")}
+          >
+            comics
+          </li>
+          <li
+            className={clicked === "movies" ? "clicked-btn" : ""}
+            onClick={() => setClicked("movies")}
+          >
+            movies
+          </li>
+          <li
+            className={clicked === "shows" ? "clicked-btn" : ""}
+            onClick={() => setClicked("shows")}
+          >
+            tv shows
+          </li>
+        </ul>
+        <div className="icons">
+          <FiSearch className="icon icon-search" />
+          <CgMenuRight className="icon icon-menu" />
         </div>
-        </nav>
-    )
-}
+      </div>
+    </nav>
+  );
+};
